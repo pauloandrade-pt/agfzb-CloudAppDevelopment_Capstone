03832ab958bc9a431a8a16a4b3713c793ed10373
Paulo Andrade <mail@pauloandrade.pt>
2022-03-02 05:01:43 +0000

    Static Pages

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,8 +10,10 @@ urlpatterns = [
     # name the URL
 
     # path for about view
+    path('about/', view=views.about, name='about'),
 
     # path for contact us view
+    path('contact/', view=views.contact, name='contact'),
 
     # path for registration
 
